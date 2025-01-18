9fc062c7549fc2198b539b6a3f4e1d7d51bbfc10
anupam <anupamminz300am@gmail.com>
2025-01-18 12:34:28 +0530

    change db schema for user and session
    alter user table to user email instead of username
    alter table sessions to gave user_id as datatype of integer

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -3,13 +3,13 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey({autoIncrement: true}),
 	age: integer('age'),
-	username: text('username').notNull().unique(),
+	email: text('email').notNull().unique(),
 	passwordHash: text('password_hash').notNull()
 });
 
 export const session = sqliteTable('session', {
 	id: integer('id').primaryKey(),
-	userId: text('user_id')
+	userId: integer('user_id')
 		.notNull()
 		.references(() => user.id),
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
