9710f0229152894ae9b52034a3b144abdf266a94
anupam <anupamminz300am@gmail.com>
2025-01-18 12:47:43 +0530

    basic signup code

---
+++ src/routes/signup/+page.server.js
@@ -0,0 +1,41 @@
+import { hash, verify } from '@node-rs/argon2';
+import { encodeBase32LowerCase } from '@oslojs/encoding';
+import { fail, redirect } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import * as auth from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+
+/** @type {import('./$types).Actions} */
+export const actions = {
+	sigunp: async ({request}) => {
+		let fullname = "";
+		let email = "";
+		let paswordHash = "";
+		try {
+			const formData = request.formData();
+			fullname = formData.get("full_name");
+			email = formData.get("email");
+			const password = formData.get("password");
+			passwordHash = await hash(password, {
+				// recommended minimum parameters
+				memoryCost: 19456,
+				timeCost: 2,
+				outputLen: 32,
+				parallelism: 1
+			});
+		} catch(err) {
+			console.error(err);
+			return fail(400, "please check the form values are filled correctly");
+		}
+		try {
+			if (fullname === "" || email === "" || passwordHash === "") {
+				return; fail(500, "something went very wrong, please contact the website admin");
+			}
+			await db.insert(table.user).values({full_name: fullname, email: email, passwordHash: passwordHash});
+		} catch(err) {
+			console.error(err);
+			return fail(409, "email already used, try logging in or clicking forget password");
+		}
+	}
+};
