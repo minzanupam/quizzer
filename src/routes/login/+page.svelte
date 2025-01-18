af0fc10838490550c471e7bc17edbfe81df02529
anupam <anupamminz300am@gmail.com>
2025-01-18 17:11:58 +0530

    add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<form action="?/login" method="POST" class="login-form">
+	<label>email <input type="email" name="email" /> </label>
+	<label>password <input type="password" name="password" /> </label>
+	<a href="/signup">don't have a password, signup here</a>
+	<button>login</button>
+</form>
+
+<style>
+	.login-form {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		padding: 1rem;
+		max-width: 640px;
+
+		label {
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+		}
+	}
+</style>
