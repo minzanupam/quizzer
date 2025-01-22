f92e3cb0709ad69712e38231ef04059fab3d873f
anupam <anupamminz300am@gmail.com>
2025-01-22 23:19:03 +0530

    start working on questions edit page

---
+++ src/routes/quiz/edit/[id]/+page.server.js
@@ -0,0 +1,18 @@
+import { error, fail, redirect } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import * as auth from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({params}) {
+	try {
+		const quiz_id = parseInt(params.id);
+		const quizs = await db.select().from(table.quiz).where(eq(table.quiz.id, quiz_id));
+		return {
+			quiz: quizs[0],
+		}
+	} catch (err) {
+		console.error(err);
+		return error(400, "failed to parse quiz_id") }
+}
--- src/routes/quiz/edit/[id]/+page.svelte
+++ src/routes/quiz/edit/[id]/+page.svelte
@@ -1,5 +1,29 @@
-<h2>{question.title}</h2>
-<form>
-	<label>question <input type="text" name="question" /></label>
-	<button> add options </button>
+<script>
+	let { data } = $props();
+</script>
+
+<h2>{data.quiz.title}</h2>
+<div>
+	{#each data.quiz.questions as question}
+		<div>
+			<h3>{question.text}</h3>
+			<div>
+				{#each question.options as option}
+					<div>
+						{option.text}
+					</div>
+				{/each}
+				<form action="?/question/option/add">
+					<input type="number" name="option" hidden value={question.id} />
+					<input type="text" name="option" />
+					<button>add options</button>
+				</form>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<form action="?/question/add" method="POST">
+	<label> <input type="text" name="question" /></label>
+	<button>add question</button>
 </form>
