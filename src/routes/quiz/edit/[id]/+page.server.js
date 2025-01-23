0b60547b3eb65f79b3a55fd04bb87a31241cc616
anupam <anupamminz300am@gmail.com>
2025-01-23 11:37:27 +0530

    aggregrate questions for quiz

--- src/routes/quiz/edit/[id]/+page.server.js
+++ src/routes/quiz/edit/[id]/+page.server.js
@@ -5,24 +5,41 @@ import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({params}) {
+export async function load({ params }) {
 	try {
 		const quiz_id = parseInt(params.id);
-		const quizs = await db.select().from(table.quiz).where(eq(table.quiz.id, quiz_id));
+		const quizzes = await db
+			.select()
+			.from(table.quiz)
+			.leftJoin(table.question, eq(table.quiz.id, table.question.quiz_id))
+			.where(eq(table.quiz.id, quiz_id));
+		const res = quizzes.reduce((acc, x) => {
+			if (acc.quiz && acc.quiz.id == x.quiz.id) {
+				acc.quiz.questions.push(x.question)
+				return acc;
+			}
+			return {
+				quiz: {
+					id: x.quiz.id,
+					title: x.quiz.title,
+					questions: [x.question]
+				}
+			}
+		}, {});
 		return {
-			quiz: quizs[0],
-		}
+			quiz: res,
+		};
 	} catch (err) {
 		console.error(err);
-		return error(400, "failed to parse quiz_id") }
+		return error(400, 'failed to parse quiz_id');
+	}
 }
 
 export const actions = {
-	question_add: async ({request}) => {
+	question_add: async ({ request }) => {
 		const formData = request.formData();
-		const question = formData.get("question");
-		await db.insert(table.question).values({text: question}).returning();
+		const question = formData.get('question');
+		await db.insert(table.question).values({ text: question }).returning();
 	},
-	option_add: async ({request}) => {
-	},
+	option_add: async ({ request }) => {}
 };
