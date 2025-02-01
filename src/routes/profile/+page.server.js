3b6fc11874362f6d863ccbd93a72efe69309b325
anupam <anupamminz300am@gmail.com>
2025-02-01 16:40:37 +0530

    display basic profile info

---
+++ src/routes/profile/+page.server.js
@@ -0,0 +1,29 @@
+import { error, fail, redirect } from '@sveltejs/kit';
+import { eq, and } from 'drizzle-orm';
+import * as auth from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+
+export async function load({ request, cookies }) {
+	const token = auth.getSessionToken(cookies);
+	if (token == "") {
+		console.error('failed to validate session, token not found');
+		return fail(400, {mesasge: 'user not logged in'});
+	}
+	const {session, user} = await auth.validateSessionToken(token);
+	if (JSON.stringify('user') == '{}') {
+		console.error('failed to validate session, user not found');
+		return fail(400, {mesasge: 'user not logged in'});
+	}
+	const db_users = await db
+		.select({
+			id: table.user.id,
+			email: table.user.email,
+			fullname: table.user.full_name
+		})
+		.from(table.user)
+		.where(eq(table.user.id, user.id));
+	return {
+		user: db_users[0],
+	};
+}
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let { data } = $props();
+
+	let editOn = $state(false);
+</script>
+
+<main>
+	<h1>Profile</h1>
+	<div>
+		{#if editOn}
+			<form action="?/edit_profile" method="POST">
+				<label>full name <input type="text" name="name" value={data.user.full_name} /></label>
+				<label>email <input type="text" name="email " value={data.user.email} /></label>
+				<button type="button">cancel</button>
+				<button>done</button>
+			</form>
+		{:else}
+			<div>{data.user.fullname}</div>
+			<div>{data.user.email}</div>
+			<button>edit profile</button>
+			<button>change password</button>
+		{/if}
+	</div>
+</main>
