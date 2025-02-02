8dc6e22c0c11ac3281b5ef1c8d367a2ae673db95
anupam <anupamminz300am@gmail.com>
2025-02-02 10:57:47 +0530

    display info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,12 @@
 		{#await data.quizzes}
 			<div>Loading...</div>
 		{:then rows}
-			{#each rows as row}
+			{#each rows as { quiz, user }}
 				<div>
-					<h3>{row.quiz.title}</h3>
-					<div>from: {row.user.full_name}</div>
-					<div>expires at: {row.quiz.expiresAt}</div>
-					<a href={`/quiz/edit/${row.quiz.id}`}> edit </a>
+					<h3>{quiz.title}</h3>
+					<div>from: {user.full_name}</div>
+					<div>expires at: {quiz.expiresAt}</div>
+					<a href={`/quiz/edit/${quiz.id}`}> edit </a>
 				</div>
 			{/each}
 		{/await}
