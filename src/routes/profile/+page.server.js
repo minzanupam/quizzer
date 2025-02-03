8e39a46f54a5fc81288b8488bf3e11fb3ca8c1c3
anupam <anupamminz300am@gmail.com>
2025-02-03 11:28:00 +0530

    update prettier config and form profile page

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,38 +1,38 @@
-import { error, fail, redirect } from '@sveltejs/kit';
-import { eq, and } from 'drizzle-orm';
-import * as auth from '$lib/server/auth';
-import { db } from '$lib/server/db';
-import * as table from '$lib/server/db/schema';
+import { error, fail, redirect } from "@sveltejs/kit";
+import { eq, and } from "drizzle-orm";
+import * as auth from "$lib/server/auth";
+import { db } from "$lib/server/db";
+import * as table from "$lib/server/db/schema";
 
 /** @type{import('./$types').PageServerLoad} */
 export async function load({ request, cookies }) {
 	const token = auth.getSessionToken(cookies);
 	if (token == "") {
-		console.error('user is not logged in, token is empty');
-		return error(401, 'user not logged in');
+		console.error("user is not logged in, token is empty");
+		return error(401, "user not logged in");
 	}
-	const {session, user} = await auth.validateSessionToken(token);
+	const { session, user } = await auth.validateSessionToken(token);
 	if (!user) {
-		console.error('user not logged in, user object is empty');
-		return error(400, 'user not logged in');
+		console.error("user not logged in, user object is empty");
+		return error(400, "user not logged in");
 	}
 	const db_users = await db
 		.select()
 		.from(table.user)
 		.where(eq(table.user.id, user.id));
 	if (db_users.length < 1) {
-		return error(500, {message: "user not found"});
+		return error(500, { message: "user not found" });
 	}
-	const {passwordHash, ...db_user} = {...db_users[0]};
+	const { passwordHash, ...db_user } = { ...db_users[0] };
 
 	return {
-		user: db_user,
+		user: db_user
 	};
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	logout: async ({cookies}) => {
+	logout: async ({ cookies }) => {
 		const token = auth.getSessionToken(cookies);
 		const { session, user } = await auth.validateSessionToken(token);
 		if (!session) {
