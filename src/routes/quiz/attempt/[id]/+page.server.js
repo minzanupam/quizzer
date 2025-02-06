6d1b6d48b58df110a0cf6738b1b38c7e6e9d1734
anupam <anupamminz300am@gmail.com>
2025-02-06 13:04:54 +0530

    query question data

--- src/routes/quiz/attempt/[id]/+page.server.js
+++ src/routes/quiz/attempt/[id]/+page.server.js
@@ -1,25 +1,55 @@
-import { error, fail, redirect } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
-import * as auth from '$lib/server/auth';
-import { db } from '$lib/server/db';
-import * as table from '$lib/server/db/schema';
+import { error, fail, redirect } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
+import * as auth from "$lib/server/auth";
+import { db } from "$lib/server/db";
+import * as table from "$lib/server/db/schema";
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({cookies, params}) {
+export async function load({ cookies, params }) {
 	const quizId = parseInt(params.id);
 	if (isNaN(quizId)) {
-		return fail(400, {message: "failed to parse quiz"});
+		return fail(400, { message: "failed to parse quiz" });
 	}
 	const token = auth.getSessionToken(cookies);
 	const { session, user } = await auth.validateSessionToken(token);
 	if (!user || !session) {
-		return fail(401, {message: "user not authenticated, please login in before continuing"});
+		return fail(401, {
+			message: "user not authenticated, please login in before continuing"
+		});
 	}
 
-	const quizzes = await db.select().from(table.quiz).where(eq(table.quiz.id, quizId));
-	if (quizzes.length == 0) {
-		return fail(404, {message: "quiz item with id not found"});
+	const rows = await db
+		.select()
+		.from(table.quiz)
+		.innerJoin(table.question, eq(table.quiz.id, table.question.quiz_id))
+		.innerJoin(table.option, eq(table.question.id, table.option.question_id))
+		.where(eq(table.quiz.id, quizId));
+	if (rows.length == 0) {
+		return fail(404, { message: "quiz item with id not found" });
 	}
+	const quiz = rows[0].quiz;
+	const question_rows = rows.map(x => x.question);
+	const questions = [];
+
+	outer: for (let q of question_rows) {
+		for (let x of questions) {
+			if (q.id == x.id) {
+				continue outer;
+			}
+		}
+		questions.push(q);
+	}
+
+	const opts = rows.map(x => x.option);
+	const options = Object.groupBy(opts, (x) => x.question_id)
+	for (let q of questions) {
+		q.options = options[q.id];
+	}
+
+	console.log(questions[0].options);
+
 	return {
+		quiz,
+		questions,
 	};
 }
--- src/routes/quiz/attempt/[id]/+page.svelte
+++ src/routes/quiz/attempt/[id]/+page.svelte
@@ -1,1 +1,18 @@
+<script>
+	/** @type {{data: import('./$types').PageServerLoad}} */
+	let { data } = $props();
+</script>
+
 <h1>Attempt the quiz</h1>
+
+<h2>One mark per qusetion, no negative marks</h2>
+<div>
+	{#each data.questions as question}
+		<div>{question.text}</div>
+		<ul>
+			{#each question.options as option}
+				<li>{option.text}</li>
+			{/each}
+		</ul>
+	{/each}
+</div>
