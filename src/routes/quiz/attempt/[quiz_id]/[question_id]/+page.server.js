4b516d8212684d1173b97e34fcaf49043ad6c047
anupam <anupamminz300am@gmail.com>
2025-03-02 13:21:56 +0530

    WIP load function for attempt quiz

---
+++ src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
@@ -0,0 +1,29 @@
+import { error, fail, redirect } from "@sveltejs/kit";
+import { eq, and } from "drizzle-orm";
+import * as auth from "$lib/server/auth";
+import { db } from "$lib/server/db";
+import * as table from "$lib/server/db/schema";
+
+/** @type{import("./$types").PageServerLoad} */
+async function load({ cookies, params }) {
+	const quizId = parseInt(params.quiz_id);
+	if (isNaN(quizId)) {
+		console.error("failed to parse quiz_id\nquiz_id value: ", quizId);
+		error(400, {message: "failed to parse quiz_id"});
+		return;
+	}
+	const questionId = parseInt(params.question_id);
+	if (isNaN(questionId)) {
+		console.error("failed to parse quiz_id\nquiz_id value: ", questionId);
+		error(400, {message: "failed to parse question_id"});
+		return;
+	}
+	const questions= await db.select().from(table.question)
+		.innerJoin(table.option, eq(table.option.question_id, table.question.id))
+		.where(
+			and(eq(table.quiz.id, quizId), eq(table.question.id, questionId)));
+
+	return {
+		question: questions[0],
+	}
+}
