1661fcd927e454384cffe4ab7ed435cf9ef2afa2
anupam <anupamminz300am@gmail.com>
2025-03-02 16:34:13 +0530

    start working on quiz attempt page

---
+++ src/routes/quiz/attempt/[quiz_id]/+page.server.js
@@ -0,0 +1,22 @@
+import { error } from "@sveltejs/kit";
+import { db } from "$lib/server/db";
+import { eq } from "drizzle-orm";
+import * as table from "$lib/server/db/schema";
+
+/** @type{import("./$types").PageServerLoad} */
+async function load({params}) {
+	const quizId = parseInt(params.quiz_id);
+	if (isNaN(quizId)) {
+		console.error("failde to parse quiz_id with value ", quizId);
+		error(400, {message: "failed to parse quiz_id"});
+	}
+	const quizzes = await db.select().from(table.quiz).where(eq(table.quiz.id, quizId));
+	try {
+		return {
+			quiz: quizzes[0],
+		}
+	} catch(err) {
+		console.error(err);
+		error(400, {"message": "failed to fetch quiz"});
+	}
+}
