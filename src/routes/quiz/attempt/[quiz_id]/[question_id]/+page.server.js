d881c31afef5fbb75ba299b8b0e2c886dca51186
anupam <anupamminz300am@gmail.com>
2025-03-03 20:01:06 +0530

    load question data

--- src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
+++ src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
@@ -1,19 +1,35 @@
 import { error } from "@sveltejs/kit";
+import { eq, and } from "drizzle-orm";
+import { db } from "$lib/server/db";
+import * as table from "$lib/server/db/schema";
 
-export async function load({cookie, params}) {
+export async function load({ cookie, params }) {
 	const quizId = parseInt(params.quiz_id);
 	const questionId = parseInt(params.question_id);
 
 	if (isNaN(quizId)) {
-		error(400, {message: "failed to parse quiz id"})
+		error(400, { message: "failed to parse quiz id" });
 	}
 	if (isNaN(questionId)) {
-		error(400, {message: "failed to parse quiz id"})
+		error(400, { message: "failed to parse quiz id" });
 	}
 
-	return {
-		question: {
-			id: 1,
-		}
+	const questions = await db
+		.select()
+		.from(table.question)
+		.where(
+			and(
+				eq(table.question.id, questionId),
+				eq(table.question.quiz_id, quizId)
+			)
+		);
+
+	try {
+		return {
+			question: questions[0]
+		};
+	} catch(err) {
+		console.error(err);
+		error(500, {message: "failed to fetch question records"});
 	}
 }
