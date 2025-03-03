bca2cbb16a97202623ed0e02d1056a59590cfea4
anupam <anupamminz300am@gmail.com>
2025-03-03 19:52:16 +0530

    remove some files

--- src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { error, fail, redirect } from "@sveltejs/kit";
-import { eq, and } from "drizzle-orm";
-import * as auth from "$lib/server/auth";
-import { db } from "$lib/server/db";
-import * as table from "$lib/server/db/schema";
-
-/** @type{import("./$types").PageServerLoad} */
-async function load({ cookies, params }) {
-}
