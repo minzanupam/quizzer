b6ef4f4de566e2a55495a118f29a22d63b46a3b3
anupam <anupamminz300am@gmail.com>
2025-03-04 13:23:38 +0530

    move to the next question

--- src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
+++ src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
@@ -1,5 +1,5 @@
 import { error, redirect } from "@sveltejs/kit";
-import { eq, and } from "drizzle-orm";
+import { eq, and, gt, sql } from "drizzle-orm";
 import { db } from "$lib/server/db";
 import * as table from "$lib/server/db/schema";
 
@@ -43,7 +43,10 @@ export async function load({ cookie, params }) {
 export const actions = {
 	next: async ({request, params, cookies}) => {
 		const formData = await request.formData();
-		console.log(formData);
+		if (!formData) {
+			console.error("form data not found, value:", formData);
+			error(400, {message: "form data not found"});
+		}
 		const quizId = parseInt(params.quiz_id);
 		if (isNaN(quizId)) {
 			console.error("failed to parse quiz id value:", params.quiz_id);
@@ -54,13 +57,31 @@ export const actions = {
 			console.error("failed to parse question id value:", formData.get("question_id"));
 			error(400, {message: "failed to parse question id"});
 		}
-		const optionId = parseInt(formData.get("question_options"));
+		const optionId = parseInt(/** @type{string} */ (formData.get("question_options")));
 		if (isNaN(optionId)) {
 			console.error("failed to parse option id: value", formData.get("question_options"));
 			error(400, {message: "failed to parse option id"});
 		}
 
-		console.log(questionId, optionId);
-		return redirect(302, `/quiz/attempt/${quizId}/${questionId}`)
+		await db.insert(table.test).values({
+			quiz_id: quizId,
+			question_id: questionId,
+			option_id: optionId,
+		});
+
+		const questions = await db
+			.select({id: table.question.id})
+			.from(table.question)
+			.where(
+				and(
+					gt(table.question.id, questionId),
+					eq(table.question.quiz_id, quizId),
+				)
+			)
+			.orderBy(sql`${table.question.id} asc`)
+			.limit(1);
+
+		const nextQuestionId = questions[0].id;
+		return redirect(302, `/quiz/attempt/${quizId}/${nextQuestionId}`);
 	}
 }
