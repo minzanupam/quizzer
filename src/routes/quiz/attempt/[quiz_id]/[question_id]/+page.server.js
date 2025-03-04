b303f73a625c8672b05ce5d536f4a3cded359c9a
anupam <anupamminz300am@gmail.com>
2025-03-04 20:39:47 +0530

    format

--- src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
+++ src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.server.js
@@ -29,13 +29,13 @@ export async function load({ cookies, params }) {
 	try {
 		return {
 			question: questions[0].question,
-			options: questions.map(x => {
+			options: questions.map((x) => {
 				return {
 					id: x.option.id,
 					text: x.option.text ?? "",
-					question_id: x.option.question_id,
+					question_id: x.option.question_id
 				};
-			}),
+			})
 		};
 	} catch (err) {
 		console.error(err);
@@ -45,41 +45,49 @@ export async function load({ cookies, params }) {
 
 /** @type{import("./$types").Actions} */
 export const actions = {
-	next: async ({request, params, cookies}) => {
+	next: async ({ request, params, cookies }) => {
 		const formData = await request.formData();
 		if (!formData) {
 			console.error("form data not found, value:", formData);
-			error(400, {message: "form data not found"});
+			error(400, { message: "form data not found" });
 		}
 		const quizId = parseInt(params.quiz_id);
 		if (isNaN(quizId)) {
 			console.error("failed to parse quiz id value:", params.quiz_id);
-			error(400, {message: "failed to parse quiz id"});
+			error(400, { message: "failed to parse quiz id" });
 		}
 		const questionId = parseInt(params.question_id);
 		if (isNaN(questionId)) {
-			console.error("failed to parse question id value:", formData.get("question_id"));
-			error(400, {message: "failed to parse question id"});
+			console.error(
+				"failed to parse question id value:",
+				formData.get("question_id")
+			);
+			error(400, { message: "failed to parse question id" });
 		}
-		const optionId = parseInt(/** @type{string} */ (formData.get("question_options")));
+		const optionId = parseInt(
+			/** @type{string} */ (formData.get("question_options"))
+		);
 		if (isNaN(optionId)) {
-			console.error("failed to parse option id: value", formData.get("question_options"));
-			error(400, {message: "failed to parse option id"});
+			console.error(
+				"failed to parse option id: value",
+				formData.get("question_options")
+			);
+			error(400, { message: "failed to parse option id" });
 		}
 
 		await db.insert(table.test).values({
 			quiz_id: quizId,
 			question_id: questionId,
-			option_id: optionId,
+			option_id: optionId
 		});
 
 		const questions = await db
-			.select({id: table.question.id})
+			.select({ id: table.question.id })
 			.from(table.question)
 			.where(
 				and(
 					gt(table.question.id, questionId),
-					eq(table.question.quiz_id, quizId),
+					eq(table.question.quiz_id, quizId)
 				)
 			)
 			.orderBy(sql`${table.question.id} asc`)
@@ -91,4 +99,4 @@ export const actions = {
 		const nextQuestionId = questions[0].id;
 		return redirect(302, `/quiz/attempt/${quizId}/${nextQuestionId}`);
 	}
-}
+};
