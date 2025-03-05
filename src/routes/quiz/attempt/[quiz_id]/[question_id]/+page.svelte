3e9223532dd58c2894770dac3e132a51984a4345
anupam <anupamminz300am@gmail.com>
2025-03-05 10:00:26 +0530

    WIP adding layout

--- src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.svelte
+++ src/routes/quiz/attempt/[quiz_id]/[question_id]/+page.svelte
@@ -2,24 +2,34 @@
 	const { data } = $props();
 </script>
 
-<h1>Question ID: {data.question?.id}</h1>
-<h3>{data.question?.text}</h3>
+<main class="page">
+	<section class="sidenav">hello</section>
+	<section class="question-section">
+		<h1>Question {data.question?.id}.</h1>
+		<div>{data.question?.text}</div>
 
-<form method="POST" action="?/next" class="question">
-	{#each data.options as option}
-		<label>
-			<input type="radio" name={`question_options`} value={option.id} />
-			{option.text}
-		</label>
-	{/each}
-	<div>
-		<button formaction="/?previous">previous</button>
-		<button type="button">clear</button>
-		<button>next</button>
-	</div>
-</form>
+		<form method="POST" action="?/next" class="question">
+			{#each data.options as option}
+				<label>
+					<input type="radio" name={`question_options`} value={option.id} />
+					{option.text}
+				</label>
+			{/each}
+			<div>
+				<button formaction="/?previous">previous</button>
+				<button type="button">clear</button>
+				<button>next</button>
+			</div>
+		</form>
+	</section>
+</main>
 
 <style>
+	.page {
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+		padding: 1rem;
+	}
 	.question {
 		display: flex;
 		flex-direction: column;
