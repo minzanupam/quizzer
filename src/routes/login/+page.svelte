4d68d5196cf1866d21942f131ad6a621d65ad136
anupam <anupamminz300am@gmail.com>
2025-03-08 10:33:59 +0530

    show login error

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,13 @@
+<script>
+	/** @type{import('./$types').PageProps}*/
+	const { form } = $props();
+</script>
+
 <form action="?/login" method="POST" class="login-form">
+	<h1>Login</h1>
+	{#if form?.message}
+		<div class="error-message">{form.message}</div>
+	{/if}
 	<label>email <input type="email" name="email" /> </label>
 	<label>password <input type="password" name="password" /> </label>
 	<a href="/signup">don't have a password, signup here</a>
@@ -18,5 +27,8 @@
 			flex-direction: column;
 			gap: 0.5rem;
 		}
+	}
+	.error-message {
+		color: red;
 	}
 </style>
