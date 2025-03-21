e9a5cbe0afb0f5aed3dc82e4df4b9c10c01e3032
anupam <anupamminz300am@gmail.com>
2025-03-21 19:21:16 +0530

    fix attempted and unattempted quizzes

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 import { error, fail, redirect } from "@sveltejs/kit";
-import { eq, ne } from "drizzle-orm";
+import { eq, ne, notInArray } from "drizzle-orm";
 import * as auth from "$lib/server/auth";
 import { db } from "$lib/server/db";
 import * as table from "$lib/server/db/schema";
@@ -24,20 +24,18 @@ export async function load({ cookies }) {
 		};
 	}
 
-	const [attempted_quizzes, unattempted_quizzes] = await Promise.all([
-		db
-			.select()
-			.from(table.quiz_attempt)
-			.innerJoin(table.quiz, eq(table.quiz_attempt.quizId, table.quiz.id))
-			.innerJoin(table.user, eq(table.quiz.ownerId, table.user.id))
-			.where(eq(table.quiz_attempt.userId, user.id)),
-		db
-			.select()
-			.from(table.quiz_attempt)
-			.innerJoin(table.quiz, eq(table.quiz_attempt.quizId, table.quiz.id))
-			.innerJoin(table.user, eq(table.quiz.ownerId, table.user.id))
-			.where(ne(table.quiz_attempt.userId, user.id))
-		]);
+	const attempted_quizzes = await db
+		.select()
+		.from(table.quiz_attempt)
+		.innerJoin(table.quiz, eq(table.quiz_attempt.quizId, table.quiz.id))
+		.innerJoin(table.user, eq(table.quiz.ownerId, table.user.id))
+		.where(eq(table.quiz_attempt.userId, user.id));
+
+	const unattempted_quizzes = await db
+		.select()
+		.from(table.quiz)
+		.innerJoin(table.user, eq(table.user.id, table.quiz.ownerId))
+		.where(notInArray(table.quiz.id, attempted_quizzes.map(x => x.quiz.id)));
 
 	return {
 		quizzes: {
